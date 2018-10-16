eb1d8e38aa62dc434e5498c5a4005438bc898a71
Amine M. Boulouma <amine@boulouma.com>
2018-10-16 17:26:05 +0200

    Added exo17

---
+++ C_exercices/Compilation/Types/exo18/exo18.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+int main()
+{
+    long a = 8000000004;
+    float b = 2;
+    long c = a / b;
+    printf("%ld, %ld, %f, %li", sizeof(float), sizeof(long), c);
+
+    return 0;
+}
