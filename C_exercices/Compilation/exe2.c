e832cf1d0be65350059c1f66208741c50faccc22
Amine M. Boulouma <amine@boulouma.com>
2018-10-16 09:08:35 +0200

    Added exo2

---
+++ C_exercices/Compilation/exe2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    unsigned int age = 22;
+    char nom[20] = "Amine Boulouma";
+    char chanson[20] = "Mairosa";
+    printf("Mon nom est %s, j'ai %u ans et j'aime %s\n", nom, age, chanson);
+}
