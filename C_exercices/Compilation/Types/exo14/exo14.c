0adb2b0267da367a43f14f5f406fd1dc90e53454
Amine M. Boulouma <amine@boulouma.com>
2018-10-16 11:44:11 +0200

    Added exo14

---
+++ C_exercices/Compilation/Types/exo14/exo14.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+int main()
+{
+    bool a = true, b = false;
+    printf("sizeof(bool) = %ld\n", sizeof(bool));
+    printf("True = %d, False = %d", a, b);
+}
