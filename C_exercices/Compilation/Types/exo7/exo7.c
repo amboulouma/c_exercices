a86e220ab613e06cf82cb610111dbf9ad4bca8f6
Amine M. Boulouma <amine@boulouma.com>
2018-10-16 10:59:27 +0200

    Added exo7

---
+++ C_exercices/Compilation/Types/exo7/exo7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <limits.h>
+
+int main()
+{
+    long i;
+    long long j;
+    printf("sizeof(long) = %ld, sizeof(long long) = %ld", sizeof(i), sizeof(j));
+}
