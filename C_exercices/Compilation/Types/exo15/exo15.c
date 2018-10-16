f46de8fc33c53b394e560b8f6243e13d2fa47b03
Amine M. Boulouma <amine@boulouma.com>
2018-10-16 11:46:50 +0200

    Added exo15

---
+++ C_exercices/Compilation/Types/exo15/exo15.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+int main()
+{
+    char char_ = 'A';
+    int val = char_ + 10;
+    printf("val = %d, char_ = %c", val, char_ + 10);
+}
