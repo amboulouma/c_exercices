582ee47833b186448bb84f5d2ebfbb61465bae9a
Amine M. Boulouma <amine@boulouma.com>
2018-10-16 09:59:21 +0200

    Added exo4

--- C_exercices/Compilation/Types/exo4/exo4.c
+++ C_exercices/Compilation/Types/exo4/exo4.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <limits.h>
 
 int main()
 {
-    char c = 128;
-    printf("%d", c);
+    unsigned int a = 0, b = -1;
+    printf("min(int) = %d\nmax(int) = %d\nmin(unsigned int) = %u\nmax(unsigned int) = %u", INT_MIN, INT_MAX, a, b);
 }
