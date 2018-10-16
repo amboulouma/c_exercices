35e72766d4e4b1889c00fecd62d918a152559a1e
Amine M. Boulouma <amine@boulouma.com>
2018-10-16 11:25:05 +0200

    Added exo10

---
+++ C_exercices/Compilation/Types/exo10/exo10.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdint.h>
+
+int main()
+{
+    uint32_t a, b = 4 * pow(10, 9), c = pow(10, 9), d = pow(10, 9);
+    a = (b + c) - d;
+    printf("%u %u", a, b);
+}
