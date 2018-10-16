8f463304b72bc73315de85a6dcfc9249bec26efb
Amine M. Boulouma <amine@boulouma.com>
2018-10-16 11:33:46 +0200

    Added exo12

---
+++ C_exercices/Compilation/Types/exo12/exo12.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdint.h>
+
+int main()
+{
+    long double pi = 3.14328979843646732647832164981264871987;
+    printf("%f", pi);
+}
