da8e52beb492e18af54b4f4e063781fd0154a092
Amine M. Boulouma <amine@boulouma.com>
2018-10-16 10:40:54 +0200

    Added exo5

---
+++ C_exercices/Compilation/Types/exo6/exo6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <limits.h>
+
+int main()
+{
+    char super = 17, bof = 10, naze = 8, superman = 22, sousleau = 2;
+    printf("super = %x, bof = %x, naze = %d, superman = %x, souleau = %x", super, bof, naze, superman, sousleau);
+}
