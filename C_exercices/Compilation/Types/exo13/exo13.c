3701499b41509c52339f3dca50b5cd555c0d672a
Amine M. Boulouma <amine@boulouma.com>
2018-10-16 11:36:57 +0200

    Added exo13

---
+++ C_exercices/Compilation/Types/exo13/exo13.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdint.h>
+
+int main()
+{
+    double pi = 3.14328979843646732647832164981264871987;
+    printf("%0.50f", pi);
+}
