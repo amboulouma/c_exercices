1d4e2d46b4ab6fd1a7a48d456b47e36de2a29261
Amine M. Boulouma <amine@boulouma.com>
2018-10-19 10:44:30 +0200

    Added operations bits a bits

---
+++ C_exercices/Compilation/Opérations bits à bits/opbitsabits.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    //Exo 22
+    char c = 42;
+    int i = 42;
+    printf("Exo 22\n%d, %c\n\n", (int)c, (char)i);
+
+    //Exo 23
+    i = -42;
+    printf("Exo 23\n%u\n", (unsigned int)i);
+    i = 0x2a3b;
+    printf("%d, %c, %c\n\n", i, (char)i, (char)0xa3b);
+    //La partie de l'entier conservée est 0xa3b
+
+    //Exo 24
+    char A = 3;
+    int B = 4;
+    float C = (float)A / B;
+    printf("Exo 24\n%f\n\n", C);
+    //conversion explicite de A en float
+}
