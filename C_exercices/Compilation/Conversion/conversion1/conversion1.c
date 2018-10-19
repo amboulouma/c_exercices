65f1a6c8d522ebaa87601f9c5b4be7973f3bd59e
Amine M. Boulouma <amine@boulouma.com>
2018-10-19 10:39:23 +0200

    Added compilation

--- C_exercices/Compilation/Conversion/conversion1/conversion1.c
+++ C_exercices/Compilation/Conversion/conversion1/conversion1.c
@@ -9,5 +9,15 @@ int main()
 
     //Exo 23
     i = -42;
-    printf("Exo 23\n%u\n\n", (unsigned int)i);
+    printf("Exo 23\n%u\n", (unsigned int)i);
+
+    i = 0x2a3b;
+    printf("%d, %c, %c\n\n", i, (char)i, (char)0xa3b);
+    //La partie de l'entier conservée est 0xa3b
+
+    //Exo 24
+    char A = 3;
+    int B = 4;
+    float C = A / B;
+    print("%f", C)
 }
