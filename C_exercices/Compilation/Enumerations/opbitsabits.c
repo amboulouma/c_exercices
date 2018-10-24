38366167fb1cab1c71a5429c608ebad663db00f9
Amine M. Boulouma <amine@boulouma.com>
2018-10-25 01:49:35 +0200

    Added Enumeration

---
+++ C_exercices/Compilation/Enumerations/opbitsabits.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdint.h>
+
+enum scrutin_e
+{
+    MAJORITAIRE = 2,
+    MAJORITAIRE_PLURI = 8,
+    PROPORTIONNEL = 42,
+    MIXTE = 12
+};
+
+typedef enum scrutin_e scrutin_t;
+
+int main()
+{
+    scrutin_t majoritaire = MAJORITAIRE;
+    scrutin_t majoritaire_pluri = MAJORITAIRE_PLURI;
+    scrutin_t proportiennel = PROPORTIONNEL;
+    scrutin_t mixte = MIXTE;
+    printf("%d %d %d %d\n", majoritaire, majoritaire_pluri, proportiennel, mixte);
+}
