d2a47f4813136a8e393034dd63d57b24deac479a
Amine M. Boulouma <amine@boulouma.com>
2018-10-25 22:41:50 +0200

    Added Enumeration without value

--- C_exercices/Compilation/Enumerations/enum.c
+++ C_exercices/Compilation/Enumerations/enum.c
@@ -5,20 +5,38 @@
 
 enum scrutin_e
 {
-    MAJORITAIRE = 2,
-    MAJORITAIRE_PLURI = 8,
-    PROPORTIONNEL = 42,
-    MIXTE = 12
+    MAJORITAIRE,
+    MAJORITAIRE_PLURI,
+    PROPORTIONNEL,
+    MIXTE
+};
+
+enum jours_e
+{
+    LUNDI,
+    MARDI,
+    MERCREDI,
+    JEUDI,
+    VENDREDI,
+    SAMEDI,
+    DIMANCHE
 };
 
 typedef enum scrutin_e scrutin_t;
+typedef enum jours_e jours_t;
 
 int main()
 {
+    // Exo 32
     scrutin_t majoritaire = MAJORITAIRE;
     scrutin_t majoritaire_pluri = MAJORITAIRE_PLURI;
     scrutin_t proportiennel = PROPORTIONNEL;
     scrutin_t mixte = MIXTE;
+
     printf("%d %d %d %d\n",
            majoritaire, majoritaire_pluri, proportiennel, mixte);
+
+    // Exo 33
+    jours_t mardi = MARDI;
+    printf("%d", mardi);
 }
