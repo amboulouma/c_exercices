25c863493b6cce210bfdacd5d8c0951d306de933
Amine M. Boulouma <amine@boulouma.com>
2018-11-10 14:07:29 +0100

    Deferencer ou pointer 1

---
+++ C_exercices/Pointeurs/deferencer_ou_pointer/deferencer_ou_pointer.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdint.h>
+
+
+
+int main()
+{
+    /*
+        1 Je déréférence ou je pointe ?
+        Chaque variable d’un programme C possède une adresse. Une adresse est un entier correspondant à
+        l’emplacement du premier octet de la mémoire occupée par la variable. Un pointeur est une variable
+        contenant une adresse.
+        L’opérateur & renvoie l’adresse de l’objet indiqué à sa suite. Un objet est un emplacement mémoire
+        identifiable. Vous pouvez par exemple demander l’adresse de n’importe quel symbole : variables locales,
+        variables globales, fonction... Exemple : &aVariable;. Il s’agit du référencement d’une variable. Mais vous
+        ne pouvez pas demander l’adresse de l’expression 5 + 1, ça n’a pas de sens en C.
+    */
+
+    return 0;
+}
