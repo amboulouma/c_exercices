6bbc58940a5ccb6370201bcf892118d6abda933c
Amine M. Boulouma <amine@boulouma.com>
2018-11-13 07:46:29 +0100

    Added deferencer ou pointer 4

--- C_exercices/Pointeurs/deferencer_ou_pointer/deferencer_ou_pointer.c
+++ C_exercices/Pointeurs/deferencer_ou_pointer/deferencer_ou_pointer.c
@@ -15,6 +15,11 @@ int main()
         - Afficher un pointeur: "%p"
         - Référencer un ponteur: &p
         - Déférencer un pointeur: *p
+        - Variable globales non initialisées: stoquées dans le BSS (basic service set) segment.
+        - Variable globales initialisées: stoquées dans le data segment.
+        - La mémoire locale aussi appelée la pile, accessible localement seulement, où sont passés les paramètres des fonctions et stockées les données locales et temporaires. (.text)
+        - La mémoire globale du segment data, accessible depuis n'importe quel endroit du programme, où sont stockées les données statiques globales. (.bss .data)
+        - La mémoire globale du tas, accessible depuis n'importe quel endroit du programme, où sont stockées les données dynamiquement allouées (malloc, réalloc, free, ...). (heap)
     */
 
     // Question 1
@@ -29,8 +34,8 @@ int main()
    printf("%p -> %d\n%p -> %d\n",&intNotInitialized, intNotInitialized, &intInitialized, intInitialized);
 
     // Question 3
-    int intNotInitialized;
-    int intInitialized = 0;
+    // int intNotInitialized;
+    // int intInitialized = 0;
 
     intNotInitialized = 42;
     intInitialized = 5;
@@ -42,6 +47,8 @@ int main()
     printf("%p -> %d\n%p -> %d\n",&p_intNotInitialized, *p_intNotInitialized, &p_intInitialized, *p_intInitialized);
     
     // Question 5
+
+
 
     return 0;
 }
