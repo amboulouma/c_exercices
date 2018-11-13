a2cfbef26506f92f5fa41bdee8c6c86d73a51f2e
Amine M. Boulouma <amine@boulouma.com>
2018-11-13 07:34:32 +0100

    Added deferencer ou pointer 4

--- C_exercices/Pointeurs/deferencer_ou_pointer/deferencer_ou_pointer.c
+++ C_exercices/Pointeurs/deferencer_ou_pointer/deferencer_ou_pointer.c
@@ -8,50 +8,19 @@ int intInitialized = 0;
 
 int main()
 {
-    /*
-        1 Je déréférence ou je pointe ?
-        Chaque variable d’un programme C possède une adresse. Une adresse est un entier correspondant à
-        l’emplacement du premier octet de la mémoire occupée par la variable. Un pointeur est une variable
-        contenant une adresse.
-        L’opérateur & renvoie l’adresse de l’objet indiqué à sa suite. Un objet est un emplacement mémoire
-        identifiable. Vous pouvez par exemple demander l’adresse de n’importe quel symbole : variables locales,
-        variables globales, fonction... Exemple : &aVariable;. Il s’agit du référencement d’une variable. Mais vous
-        ne pouvez pas demander l’adresse de l’expression 5 + 1, ça n’a pas de sens en C.
+
+    /* Notes
+        - Les différentes parties de la mémoires
+        - Afficher un pointeur: "%p"
+        - Référencer un ponteur: &p
+        - Déférencer un pointeur: *p
     */
 
-    /*
-        QUESTION 1 I Écrivez un programme qui affiche l’adresse de votre fonction main(). Dans quelle section
-        de la mémoire (Figure 2.1) se trouve cette fonction ?
-    */
+    // Question 1
     
     printf("%p\n", main);
 
-    /*
-        QUESTION 2 I Écrivez un programme qui :
-        — affecte à une variable globale intNotInitialized non initialisée| la valeur 42
-        — affecte à une variable globale intInitialized initialisée à zéro la valeur 5
-        — affiche les valeurs et l’adresse de ces variables ainsi que l’adresse de la variable.
-        11
-        À quelle section de la Figure 2.1 chacune de ses variable appartient-elle ? Vérifiez que ces adresses ainsi
-        que l’adresse de la fonction main() sont ordonnées correctement, c’est à dire qu’une adresse appartenant
-        à la section .text n’est pas supérieure à une autre.
-        Une adresse peut être stockée dans une variable mais cette variable doit avoir un type adéquat. Ce type est
-        indiqué par le type de l’élément en mémoire suivi du caractère *. On l’appelle alors un pointeur. Exemple de
-        déclaration d’un pointeur : int* adress;. Exemple d’affectation de ce pointeur : adress = &aVariable
-    */
-
-   /*
-        Q UESTION 2 I Écrivez un programme qui :
-        — affecte à une variable globale intNotInitialized non initialisée| la valeur 42
-        — affecte à une variable globale intInitialized initialisée à zéro la valeur 5
-        — affiche les valeurs et l’adresse de ces variables ainsi que l’adresse de la variable.
-        11À quelle section de la Figure 2.1 chacune de ses variable appartient-elle ? Vérifiez que ces adresses ainsi
-        que l’adresse de la fonction main() sont ordonnées correctement, c’est à dire qu’une adresse appartenant
-        à la section .text n’est pas supérieure à une autre.
-        Une adresse peut être stockée dans une variable mais cette variable doit avoir un type adéquat. Ce type est
-        indiqué par le type de l’élément en mémoire suivi du caractère * . On l’appelle alors un pointeur. Exemple de
-        déclaration d’un pointeur : int* adress; . Exemple d’affectation de ce pointeur : adress = &aVariable
-   */
+   // Question 2
 
    intNotInitialized = 42;
    intInitialized = 5;
@@ -59,7 +28,19 @@ int main()
    printf("%p -> %d\n%p -> %d\n",&intNotInitialized, intNotInitialized, &intInitialized, intInitialized);
 
     // Question 3
-    
+    int intNotInitialized;
+    int intInitialized = 0;
 
-   return 0;
+    intNotInitialized = 42;
+    intInitialized = 5;
+    printf("%p -> %d\n%p -> %d\n",&intNotInitialized, intNotInitialized, &intInitialized, intInitialized);
+
+    // Question 4
+    int* p_intNotInitialized = &intNotInitialized;
+    int* p_intInitialized = &intInitialized;
+    printf("%p -> %d\n%p -> %d\n",&p_intNotInitialized, *p_intNotInitialized, &p_intInitialized, *p_intInitialized);
+    
+    // Question 5
+
+    return 0;
 }
